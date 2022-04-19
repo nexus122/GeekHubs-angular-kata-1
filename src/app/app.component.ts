883fbcea5df6647ea799416ebf8b6fbd96ca0279
nexus122 <juan-pablo122@hotmail.es>
2022-04-20 01:19:37 +0200

    Kata 1 Solución 3 componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 
+import { HeaderComponent } from './header/header.component';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
+
+
+
 export class AppComponent {
   title = 'GeekHubs-angular-kata-1';
 }
